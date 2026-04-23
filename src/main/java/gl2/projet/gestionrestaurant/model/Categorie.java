e0250eaf216c31531a77c9754e5391e899aae3a6
WalaSelmiii <walaselmi20@gmail.com>
2026-04-23 15:13:56 +0100

    les tablesss

--- src/main/java/gl2/projet/gestionrestaurant/model/Categorie.java
+++ src/main/java/gl2/projet/gestionrestaurant/model/Categorie.java
@@ -1,4 +1,49 @@
 package gl2.projet.gestionrestaurant.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 
 public class Categorie {
-}
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+        private String nom;
+        private String description;
+
+        public Categorie() {}
+
+        public Categorie(String nom, String description) {
+            this.nom = nom;
+            this.description = description;
+        }
+
+        // Getters and Setters
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNom() {
+            return nom;
+        }
+
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description ) {
+            this.description = description;
+        }
+
+    }
--- src/main/java/gl2/projet/gestionrestaurant/model/CommandeLigne.java
+++ src/main/java/gl2/projet/gestionrestaurant/model/CommandeLigne.java
@@ -1,4 +1,65 @@
 package gl2.projet.gestionrestaurant.model;
+import jakarta.persistence.*;
 
+@Entity
 public class CommandeLigne {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String note;
+    private int quantité;
+
+    // clé etrangere plat
+    @ManyToOne
+    @JoinColumn(name = "plat_id")
+    private Plat plat;
+
+    // clé etrangere commande
+    @ManyToOne
+    @JoinColumn(name = "commande_id")
+    private Commande commande;
+
+    //Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public int getQuantité() {
+        return quantité;
+    }
+
+    public void setQuantité(int quantité) {
+        this.quantité = quantité;
+    }
+
+    public Plat getPlat() {
+        return plat;
+    }
+
+    public void setPlat(Plat plat) {
+        this.plat = plat;
+    }
+
+    public Commande getCommande() {
+        return commande;
+    }
+
+    public void setCommande(Commande commande) {
+        this.commande = commande;
+    }
 }
