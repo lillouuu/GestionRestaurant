268be4a704eec8489f101e2bfc745f7f48df81af
lou <louaklai98@gmail.com>
2026-04-23 14:29:13 +0100

    initial repo

---
+++ src/main/java/gl2/projet/gestionrestaurant/GestionRestaurantApplication.java
@@ -0,0 +1,13 @@
+package gl2.projet.gestionrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionRestaurantApplication.class, args);
+    }
+
+}
